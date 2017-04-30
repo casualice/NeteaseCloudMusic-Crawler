684ccb10c0f86adacd6c00044f2a6d7bdde919c6
randomicecream <403834475@qq.com>
2017-04-30 22:10:57 +0800

    添加一个新的Spring demo

--- SpringBoot/src/main/java/com/example/controllor/IndexController.java
+++ SpringBoot/src/main/java/com/example/controllor/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by 40383 on 2017/4/20.
